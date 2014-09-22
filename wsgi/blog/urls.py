fac0f4ce7dd19587ff341cd38c57f99602ccbbb8
evilbinary <rootntsd@gmail.com>
2014-09-22 19:17:36 +0800

    modify

--- wsgi/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import *
-from blog.views import * 
-from django.conf import settings
-
-urlpatterns= patterns('',
-            url(r'^$',index),
-            url(r'^blog/',index),
-            url(r'^archive',archive),
-            )
