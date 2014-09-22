38a33ba8c8f13af19f7a04d0a5770420277c5f8f
evilbinary <rootntsd@gmail.com>
2014-09-22 22:58:51 +0800

    modify urls

--- wsgi/mysite/mysite/urls.py
+++ wsgi/mysite/mysite/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
-    url(r'^/', include('blog.urls')),
+    url(r'^$', include('blog.urls')),
     url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
