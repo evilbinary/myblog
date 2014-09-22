568c10a683ae339d9fce0612ef62cf1bc4ab3a84
evilbinary <rootntsd@gmail.com>
2014-09-22 18:37:01 +0800

    modify

--- wsgi/mysite/mysite/settings.py
+++ wsgi/mysite/mysite/settings.py
@@ -59,15 +59,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default':{
+    'default2':{
         'ENGINE': 'django.db.backends.mysql',
         'NAME' : 'django',
-        'USER' : 'adminjvX7IiV',
-        'PASSWORD' : '63KI924gTaE_',
+        'USER' : '',
+        'PASSWORD' : '',
         'HOST' : '127.0.0.1',
         'PORT' : '3306'
     },
-    'default1': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
