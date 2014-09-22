fbdd13bbc1945dc8557121a4d10c38169c28e7d7
evilbinary <rootntsd@gmail.com>
2014-09-22 17:55:20 +0800

    add code

--- wsgi/mysite/settings.py
+++ wsgi/mysite/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ON_OPENSHIFT = False
+if os.environ.has_key('OPENSHIFT_REPO_DIR'):
+        ON_OPENSHIFT = True
 
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
