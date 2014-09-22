c84b8b3689b5e393c5a15f75da564b2af0381ad4
evilbinary <rootntsd@gmail.com>
2014-09-22 23:35:39 +0800

    modify

--- wsgi/mysite/mysite/settings.py
+++ wsgi/mysite/mysite/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = 'blog/static/'
+STATIC_URL = '/static/'
 
 TEMPLATE_DIR=(
         os.path.join(os.path.dirname(__file__), '../blog/templates').replace('\\','/'),
