a89d1d54152e64f9640f01508ffdc6940ae0c2f9
evilbinary <rootntsd@gmail.com>
2015-01-06 22:32:15 +0800

    修改bug

--- wsgi/mysite/blog/views.py
+++ wsgi/mysite/blog/views.py
@@ -465,36 +465,26 @@ def render_article(request,post_id,contexts=None):
     # post_id=int(post_id)
     #incre views
     vv=request.session.get('post_views')
-    # print vv,' vv.has_key(post_id):',vv.has_key(post_id),' post_id:',post_id
-    if vv:
-        if vv.has_key(post_id):
-            pass
-        else:
-
-            views=Postmeta.objects.filter(post_id=cur_post.first(),meta_key='views').first()
-            if views:
-                views.meta_value=str(int(views.meta_value)+1)
-                views.save()
-            request.session.modified = True
-            request.session['post_views'][post_id]=1
-            # print vv,' vv.has_key(post_id):',vv.has_key(post_id),' post_id:',post_id
-
-            # print request.session['post_views']
-        pass
-    else:
+    if vv==None:
+        request.session.modified = True
+        request.session['post_views'] ={}
+        vv=request.session.get('post_views')
+    if vv.has_key(post_id)==False:
+        print 2
         views=Postmeta.objects.filter(post_id=cur_post.first(),meta_key='views').first()
         if views:
             views.meta_value=str(int(views.meta_value)+1)
             views.save()
+            print 3
         else:
             views=Postmeta()
             views.post_id=cur_post.first()
             views.meta_key=u'views'
-            views.meta_value='0'
+            views.meta_value='1'
             views.save()
-            pass
-        request.session['post_views']={post_id:1};
-        print id(request.session['post_views'])
+            print 4
+        request.session.modified = True
+        request.session['post_views'][post_id]=1
 
 
     #comment_author=request.POST.get('author')
