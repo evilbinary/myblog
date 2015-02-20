273b0da684e0083f56f57e9c73f156bf1aad13bc
evilbinary <rootntsd@gmail.com>
2015-02-20 10:41:10 +0800

    修复挂掉问题

--- wsgi/mysite/mysite/settings.py
+++ wsgi/mysite/mysite/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'ckeditor',
 
 )
 
--- wsgi/mysite/mysite/urls.py
+++ wsgi/mysite/mysite/urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
     url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^ckeditor/', include('ckeditor.urls')),
+
 )
