60b371195986c791e4bd9af8a8199763684ee86c
evilbinary <rootntsd@gmail.com>
2017-10-08 01:16:29 +0800

    modify path

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#   Author  :   cold
+#   E-mail  :   rootntsd@gmail.com
+#   Date    :   14/10/1 12:21:19
+#   Desc    :   
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.mysite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
     from django.core.management import execute_from_command_line
 
