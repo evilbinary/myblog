d16a64af0c860475a3853e65fb851666dc5acb5a
evilbinary <rootntsd@gmail.com>
2017-10-08 01:23:57 +0800

    setting path

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
